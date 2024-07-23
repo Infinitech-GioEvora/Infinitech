149cb1a80044a5a218b76fa6cd75ad303808319a
Infinitech-GioEvora <infinitech.giolo.evora@gmail.com>
2024-07-23 18:35:08 +0800

    ..

--- resources/views/Homepage/ContactUs.blade.php
+++ resources/views/Homepage/ContactUs.blade.php
@@ -15,10 +15,11 @@
     <meta name="author" content="Infinitech Advertising Corporation">
 
     <!-- Favicon (example: using a 32x32 PNG favicon named 'favicon.png' in the root directory) -->
-    <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/Favicon.svg">
-    <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicon.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../img/favicon-16x16.png">
-
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    
     <!-- CSS Stylesheet (example: linking to a stylesheet named 'styles.css' in the 'css' directory) -->
     <link rel="stylesheet" href="/assets/css/style.css">
 
