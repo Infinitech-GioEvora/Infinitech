2ae69ee8b9968d07fc6162647d6ab7b52632e9ec
gioevora <giolo.evora@gmail.com>
2024-09-03 18:28:41 +0800

    update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -216,7 +216,7 @@ class MainController extends Controller
     
     
         $filename = $user->lastname . '-' . $user->firstname . '.vcf';
-        $filePath = $vCardDirectory . $filename; 
+        $filePath = $vCardDirectory . DIRECTORY_SEPARATOR . $filename; 
     
     
         $vCard->setSavePath($vCardDirectory);
