cbac77f949241b489970dbffa1da65bed0eebe5e
Kimberly <infinitech.kimberly@gmail.com>
2024-09-13 15:11:48 +0800

    up

--- resources/views/Credential/SirJustin.Blade.php
+++ resources/views/Credential/SirJustin.Blade.php
@@ -10,10 +10,10 @@
 </head>
 
 <body>
-     <main>
+    <main>
         <section class="hero contact-layout">
-          <div class="hero-overlay"></div>
-          <button id="myButton"></button> 
+            <div class="hero-overlay"></div>
+            <button id="myButton"></button>
             <div class="container contact-section">
 
                 <div class="row">
@@ -89,43 +89,42 @@
                       </div>
                     </div>
                 </div>
-
-                    <div class="col-12 col-md-12 col-lg-6">
-                      <div class="left-content">
-                          <div class="slider">
-                            <div class="item">
-                              <img src="/assets/img/cred/j1.png">
-                          </div>
-                          <div class="item">
-                              <img src="/assets/img/cred/j2.png" >
-                          </div>
-                          <div class="item">
-                              <img src="/assets/img/cred/j3.png">
-                          </div>
-                          <div class="item">
-                              <img src="/assets/img/cred/j4.png">
-                          </div>
-                          <div class="item">
-                              <img src="/assets/img/cred/j5.png" >
-                          </div>
-                          <div class="item">
-                            <img src="/assets/img/cred/j1.png">
-                          </div>
-                          <div class="item">
-                              <img src="/assets/img/cred/j2.png" >
-                          </div>
-                          <div class="item">
-                              <img src="/assets/img/cred/j3.png">
-                          </div>
-                          <div class="item">
-                              <img src="/assets/img/cred/j4.png">
-                          </div>
-                          <div class="item">
-                              <img src="/assets/img/cred/j5.png" >
-                          </div>
-                            <button id="next">></button>
-                            <button id="prev"><</button>
-                        </div>
+                    <div class="col-12 col-md-6">
+                        <div class="left-content">
+                            <div class="slider">
+                                <div class="item">
+                                    <img src="/assets/img/cred/j1.png">
+                                </div>
+                                <div class="item">
+                                    <img src="/assets/img/cred/j2.png" >
+                                </div>
+                                <div class="item">
+                                    <img src="/assets/img/cred/j3.png">
+                                </div>
+                                <div class="item">
+                                    <img src="/assets/img/cred/j4.png">
+                                </div>
+                                <div class="item">
+                                    <img src="/assets/img/cred/j5.png" >
+                                </div>
+                                <div class="item">
+                                    <img src="/assets/img/cred/j1.png">
+                                </div>
+                                <div class="item">
+                                    <img src="/assets/img/cred/j2.png" >
+                                </div>
+                                <div class="item">
+                                    <img src="/assets/img/cred/j3.png">
+                                </div>
+                                <div class="item">
+                                    <img src="/assets/img/cred/j4.png">
+                                </div>
+                                <div class="item">
+                                    <img src="/assets/img/cred/j5.png" >
+                                </div>
+                                <button id="next">></button>
+                                <button id="prev"><</button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -133,81 +132,82 @@
         </section>
 
         <section id="gallery">
-          <div class="container">
-            <div id="image-gallery">
-              <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                  <div class="img-wrapper">
-                    <a href="https://www.viit.ac.in/images/Campus-life/Institute-Innovation-Council/Certificate-2020-21.png"><img src="https://www.viit.ac.in/images/Campus-life/Institute-Innovation-Council/Certificate-2020-21.png" class="img-responsive"></a>
-                    <div class="img-overlay">
-                      <i class="fa fa-plus-circle" aria-hidden="true"></i>
+            <div class="container">
+                <div id="image-gallery">
+                  <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                      <div class="img-wrapper">
+                        <a href="https://www.viit.ac.in/images/Campus-life/Institute-Innovation-Council/Certificate-2020-21.png"><img src="https://www.viit.ac.in/images/Campus-life/Institute-Innovation-Council/Certificate-2020-21.png" class="img-responsive"></a>
+                        <div class="img-overlay">
+                          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                  <div class="img-wrapper">
-                    <a href="https://edit.org/images/cat/diplomas-certificates-big-2020042416.jpg"><img src="https://edit.org/images/cat/diplomas-certificates-big-2020042416.jpg" class="img-responsive"></a>
-                    <div class="img-overlay">
-                      <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                      <div class="img-wrapper">
+                        <a href="https://edit.org/images/cat/diplomas-certificates-big-2020042416.jpg"><img src="https://edit.org/images/cat/diplomas-certificates-big-2020042416.jpg" class="img-responsive"></a>
+                        <div class="img-overlay">
+                          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                  <div class="img-wrapper">
-                    <a href="https://d3s6gs1cfdg3qb.cloudfront.net/files/95633c9c3482e975719cf0adf923831bec55cc747adc5ced79043941730da2c3.jpg"><img src="https://d3s6gs1cfdg3qb.cloudfront.net/files/95633c9c3482e975719cf0adf923831bec55cc747adc5ced79043941730da2c3.jpg" class="img-responsive"></a>
-                    <div class="img-overlay">
-                      <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                      <div class="img-wrapper">
+                        <a href="https://d3s6gs1cfdg3qb.cloudfront.net/files/95633c9c3482e975719cf0adf923831bec55cc747adc5ced79043941730da2c3.jpg"><img src="https://d3s6gs1cfdg3qb.cloudfront.net/files/95633c9c3482e975719cf0adf923831bec55cc747adc5ced79043941730da2c3.jpg" class="img-responsive"></a>
+                        <div class="img-overlay">
+                          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                  <div class="img-wrapper">
-                    <a href="https://png.pngtree.com/png-clipart/20220430/original/pngtree-diploma-certificate-template-png-image_7598348.png"><img src="https://png.pngtree.com/png-clipart/20220430/original/pngtree-diploma-certificate-template-png-image_7598348.png" class="img-responsive"></a>
-                    <div class="img-overlay">
-                      <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                      <div class="img-wrapper">
+                        <a href="https://png.pngtree.com/png-clipart/20220430/original/pngtree-diploma-certificate-template-png-image_7598348.png"><img src="https://png.pngtree.com/png-clipart/20220430/original/pngtree-diploma-certificate-template-png-image_7598348.png" class="img-responsive"></a>
+                        <div class="img-overlay">
+                          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                  <div class="img-wrapper">
-                    <a href="https://suneducationgroup.com/wp-content/uploads/2023/12/CONTOH-POSTGRADUATE-CERTIFICATE-1024x730.jpg.webp"><img src="https://suneducationgroup.com/wp-content/uploads/2023/12/CONTOH-POSTGRADUATE-CERTIFICATE-1024x730.jpg.webp" class="img-responsive"></a>
-                    <div class="img-overlay">
-                      <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                      <div class="img-wrapper">
+                        <a href="https://suneducationgroup.com/wp-content/uploads/2023/12/CONTOH-POSTGRADUATE-CERTIFICATE-1024x730.jpg.webp"><img src="https://suneducationgroup.com/wp-content/uploads/2023/12/CONTOH-POSTGRADUATE-CERTIFICATE-1024x730.jpg.webp" class="img-responsive"></a>
+                        <div class="img-overlay">
+                          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                  <div class="img-wrapper">
-                    <a href="https://www.4icu.org/i/programs-courses-degrees/master-of-education-in-ict-500x356.png"><img src="https://www.4icu.org/i/programs-courses-degrees/master-of-education-in-ict-500x356.png " class="img-responsive"></a>
-                    <div class="img-overlay">
-                      <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                      <div class="img-wrapper">
+                        <a href="https://www.4icu.org/i/programs-courses-degrees/master-of-education-in-ict-500x356.png"><img src="https://www.4icu.org/i/programs-courses-degrees/master-of-education-in-ict-500x356.png " class="img-responsive"></a>
+                        <div class="img-overlay">
+                          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                  <div class="img-wrapper">
-                    <a href="https://www.4icu.org/i/programs-courses-degrees/master-of-applied-computer-science-500x356.png"><img src="https://www.4icu.org/i/programs-courses-degrees/master-of-applied-computer-science-500x356.png" class="img-responsive"></a>
-                    <div class="img-overlay">
-                      <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                      <div class="img-wrapper">
+                        <a href="https://www.4icu.org/i/programs-courses-degrees/master-of-applied-computer-science-500x356.png"><img src="https://www.4icu.org/i/programs-courses-degrees/master-of-applied-computer-science-500x356.png" class="img-responsive"></a>
+                        <div class="img-overlay">
+                          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
-                  <div class="img-wrapper">
-                    <a href="https://www.4icu.org/i/programs-courses-degrees/master-of-history-500x356.png"><img src="https://www.4icu.org/i/programs-courses-degrees/master-of-history-500x356.png" class="img-responsive"></a>
-                    <div class="img-overlay">
-                      <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 image">
+                      <div class="img-wrapper">
+                        <a href="https://www.4icu.org/i/programs-courses-degrees/master-of-history-500x356.png"><img src="https://www.4icu.org/i/programs-courses-degrees/master-of-history-500x356.png" class="img-responsive"></a>
+                        <div class="img-overlay">
+                          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </div><!-- End row -->
-            </div><!-- End image gallery -->
-          </div><!-- End container --> 
-          </section>
+                  </div><!-- End row -->
+                </div><!-- End image gallery -->
+              </div><!-- End container --> 
+        </section>
 
         @include('Layout.Social')
     </main>
 
+    <script src="/Credential.js"></script>
     <script src="/Slider.js"></script>
 </body>
 @include('Layout.Script')
