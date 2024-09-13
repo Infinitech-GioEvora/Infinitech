2e73e75523fd26a02cf69bcc16353e90b9b86cba
Kimberly <infinitech.kimberly@gmail.com>
2024-09-13 15:22:36 +0800

    test

--- resources/views/Credential/SirJustin.Blade.php
+++ resources/views/Credential/SirJustin.Blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Credential - Infinitech Advertising Corporation</title>
+    <title>Credentialsssssssss - Infinitech Advertising Corporation</title>
 
     @include('Layout/Header')
 </head>
