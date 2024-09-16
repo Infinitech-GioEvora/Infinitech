8e0fc54e1a253101a683b358e98a2c3bb224f057
Kimberly <infinitech.kimberly@gmail.com>
2024-09-16 08:24:39 +0800

    up

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -193,7 +193,7 @@ class MainController extends Controller
     public function downloadVCard(Request $request)
     {
         $id = $request->input('id'); 
-        $user = Employees::where('employeeID', '24-0002')->first();
+        $user = Employees::where('employeeID', $id)->first();
         
         if (!$user) {
             return response()->json(['error' => 'User not found.']);
@@ -203,7 +203,7 @@ class MainController extends Controller
         $vCard = new VCard();
         $vCard->addName($fname, $user->lastname);
         $vCard->addEmail($user->email, $type='Email');
-        $vCard->addPhoneNumber($user->phone, $type='WORK');
+        $vCard->addPhoneNumber($user->phonenumber, $type='WORK');
         $vCard->addJobtitle("Infinitech Advertising Corporation - ". $user->position);
         $vCard->addAddress($name = '', $extended = 'Unit 311, Campos Rueda Bldg.', $street = 'Urban Ave.', $city ='Makati City', $region='NCR', $zip='5200', $country='Philippines', $type='WORK');
         $vCard->addURL("https://infinitechphil.com/");
